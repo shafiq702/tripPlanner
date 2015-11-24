3f3e11f1851564309a662854486cbbe08ee2028b
Shafiq Marediya <shafiq702@gmail.com>
2015-11-24 13:31:23 -0500

    setup seed and mongo

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var Place = models.Page;
+var Hotel = models.Hotel;
+var Activity = models.Activity;
+var Restaurant = models.Restaurant;
 
 router.get('/', function(req, res, next){
-  res.render('layout');
+	Hotel.find({}).exec().then(function(restaurants){
+			Activity.find({}).exec().then(function(activities){
+				res.render('index', {
+					all_hotels: hotels,
+					all_restaurants: restaurants,
+					all_activities: activities
+				})
+			}).then(null, console.log());
+		}).then(null, console.log());
+	}).then(null, console.log());
 });
 
 module.exports = router;
