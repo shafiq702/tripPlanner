b4d0ef2c50b1f8fafec842d970d80823de9a2f92
Shafiq Marediya <shafiq702@gmail.com>
2015-11-24 14:50:09 -0500

    add swig to index

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var Place = models.Page;
+var models = require('../models');
 var Hotel = models.Hotel;
 var Activity = models.Activity;
 var Restaurant = models.Restaurant;
 
 router.get('/', function(req, res, next){
-	Hotel.find({}).exec().then(function(restaurants){
-			Activity.find({}).exec().then(function(activities){
-				res.render('index', {
-					all_hotels: hotels,
-					all_restaurants: restaurants,
-					all_activities: activities
-				})
-			}).then(null, console.log());
-		}).then(null, console.log());
-	}).then(null, console.log());
+	Hotel.find({}).exec().then(function(hotels) {
+    Restaurant.find({}).exec().then(function(restaurants) {
+        Activity.find({}).exec().then(function(activities) {
+            res.render('index', {
+                all_hotels: hotels,
+                all_restaurants: restaurants,
+                all_activities: activities
+            });
+        }).then(null, console.log);
+    }).then(null, console.log);
+}).then(null, console.log);
 });
 
 module.exports = router;
