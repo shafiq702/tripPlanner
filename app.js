b13151c7094f743abe1466cef97848060f3d2a60
Shafiq Marediya <shafiq702@gmail.com>
2015-11-24 11:05:35 -0500

    added html template

--- app.js
+++ app.js
@@ -6,10 +6,23 @@ var express = require('express'),
 
 var app = express();
 
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+swig.setDefaults({ cache: false });
+
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(require('./routes/routes'));
+
+app.use(function(err, req, res, next){
+	console.log(err);
+	res.status(500).send(err.message);
+});
 
 app.listen(3000, function(err){
   console.log('Listening to port 3000');
