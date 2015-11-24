c2493e762fc2054c2666c5954e5596f8097baeab
Lily Jen <lily@lilyjen.com>
2015-11-24 11:27:45 -0500

    more merge conflicts fixed

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
 router.get('/', function(req, res, next, err){
   res.render('layout');
 });
-=======
-router.get('/', function(req, res){
-	res.render('layout');
-})
->>>>>>> 0169d28890150e8328eca0095054e91906466ad8
 
 module.exports = router;
